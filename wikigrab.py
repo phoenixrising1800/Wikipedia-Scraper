281b6c327b203cd0ce707d9afe27c2d9ce9827fb
Nixy Camacho <phoenixrising1800@gmail.com>
2023-10-07 06:43:27 -0400

    Added user page input functionality and helpful printing statements

--- wikigrab.py
+++ wikigrab.py
@@ -4,11 +4,10 @@
         Github: @phoenixrising1800
 """
 
-import urllib.request
+import urllib.request, urllib.error
 import re
 
 url = 'https://en.wikipedia.org/wiki/'
-page ='Ring_species' # this is to be changed
 
 # Helper function to easily get index containing certain string
 def getEleIndex(lst: list, s: str) -> int:
@@ -29,37 +28,53 @@ def makeString(lst: list) -> str:
         s = s + ele
     return s
 
+def setUserPage():
+    return input("Type in name of page (or ENTER for random): ")
+
+# Prompt user to input valid Wiki page or get from cli args
+page = setUserPage()
+if page == '':
+    page ='Ring_species' # default
+
 # Send HTML GET Req query to url page
-req = urllib.request.Request(url+page)
-res = urllib.request.urlopen(req)
-html = res.read()
-htmlStr = str(html, 'utf-8')
-htmlList = htmlStr.splitlines(True) # List of HTML document lines [0-(n-1 index]
+try: 
+    req = urllib.request.Request(url+page)
+    res = urllib.request.urlopen(req)
+    print("* Accessed Wiki page: \n\t" + (url+page) + "--------------------\n")
+    html = res.read()
+    htmlStr = str(html, 'utf-8')
+    htmlList = htmlStr.splitlines(True) # List of HTML document lines [0-(n-1 index]
 
-# Write initial response html to file 'output.txt' (for testing purposes)
-with open('output0.txt', 'w+') as f:
+    # Write initial response html to file 'output.txt' (for testing purposes)
+    with open('output0.txt', 'w+') as f:
+        for line in htmlList:
+            f.write(line)
+    print("* HTTP GET Response HTML Data saved to ./output0.txt")
+
+    patTitle = '<title>(.*)</title>'
+    patPara = r'\/?[\"\w+]>([\w\s\,\(\)\;\.\"\-\']*)\s?<?'
+
+    matchTitle = re.findall(patTitle, htmlStr)[0] # get 1st title found in tags
+    matches = [] # 2d list of matching lines (stored in a list)
     for line in htmlList:
-        f.write(line)
+        tmp = re.findall(patPara, line)
+        matches.append(tmp)
 
-patTitle = '<title>(.*)</title>'
-patPara = r'\/?[\"\w+]>([\w\s\,\(\)\;\.\"\-\']*)\s?<?'
+    # Remove empty, single whitespace elements etc.
+    matches = [[e for e in line if e != '' and e != ' ' and e != 'citation needed'] for line in matches]
+    # Remove unneccessary elements from list
+    matches = [line for line in matches if line != [] and line != ['\n']] # remove empty lists/line ele
+    # Remove lines with smaller len than 2 (most likely not useful)
+    matches = [line for line in matches if len(line) >= 5]
+    print("* HTML data successfully scraped")
 
-matchTitle = re.findall(patTitle, htmlStr)[0] # get 1st title found in tags
-matches = [] # 2d list of matching lines (stored in a list)
-for line in htmlList:
-    tmp = re.findall(patPara, line)
-    matches.append(tmp)
+    with open('output.txt', 'w+') as f: # write matching content lines to outputf
+        f.write(matchTitle + '\n')
+        for line in matches:
+            lineStr = makeString(line)
+            f.write(lineStr)
+    print("* Scraped HTML Data saved to ./output.txt")
 
-# Remove empty, single whitespace elements etc.
-matches = [[e for e in line if e != '' and e != ' ' and e != 'citation needed'] for line in matches]
-# Remove unneccessary elements from list
-matches = [line for line in matches if line != [] and line != ['\n']] # remove empty lists/line ele
-# Remove lines with smaller len than 2 (most likely not useful)
-matches = [line for line in matches if len(line) >= 5]
-
-with open('output.txt', 'w+') as f: # write matching content lines to outputf
-    f.write(matchTitle + '\n')
-    for line in matches:
-        lineStr = makeString(line)
-        f.write(lineStr)
-
+except urllib.error.HTTPError as e: 
+    print(e)
+    print("Invalid Wiki page, try again.\n------------------\n")
